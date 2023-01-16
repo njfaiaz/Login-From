d219f687d66930ab943ad1e0430e4e3948e0ff24
NjFaiaz <njfaiaz@gmail.com>
2023-01-16 22:33:19 +0600

    registration form cascading style Auto division & Select

---
+++ Login 01/registration form cascading style  Or DorpeDown division/countries.js
@@ -0,0 +1,30 @@
+
+
+var country_arr = new Array("Afghanistan", "Albania", "Algeria", "American Samoa", "Angola", "Anguilla", "Antartica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Ashmore and Cartier Island", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burma", "Burundi", "Cambodia");
+
+var s_a = new Array();
+s_a[0]="";
+s_a[1]="Badakhshan|Badghis|Baghlan|Balkh|Bamian|Farah|Faryab|Ghazni|Ghowr|Helmand|Herat|Jowzjan|Kabol|Kandahar|Kapisa|Konar|Kondoz|Laghman|Lowgar|Nangarhar|Nimruz|Oruzgan|Paktia|Paktika|Parvan|Samangan|Sar-e Pol|Takhar|Vardak|Zabol";
+
+
+function print_country(country_id){
+	// given the id of the <select> tag as function argument, it inserts <option> tags
+	var option_str = document.getElementById(country_id);
+	option_str.length=0;
+	option_str.options[0] = new Option('Select Country','');
+	option_str.selectedIndex = 0;
+	for (var i=0; i<country_arr.length; i++) {
+		option_str.options[option_str.length] = new Option(country_arr[i],country_arr[i]);
+	}
+}
+
+function print_state(state_id, state_index){
+	var option_str = document.getElementById(state_id);
+	option_str.length=0;	// Fixed by Julian Woods
+	option_str.options[0] = new Option('Select State','');
+	option_str.selectedIndex = 0;
+	var state_arr = s_a[state_index].split("|");
+	for (var i=0; i<state_arr.length; i++) {
+		option_str.options[option_str.length] = new Option(state_arr[i],state_arr[i]);
+	}
+}
